0aeccd6e5832d7233ec5c49cfd73f086baa75049
JeremyChen1 <yc1921@ic.ac.uk>
2025-02-23 22:41:42 +0000

    Updated .gitignore to exclude Visual Studio files

---
+++ test_env.py
@@ -0,0 +1,11 @@
+import numpy as np
+import torch
+import nibabel as nib
+import dosma
+
+print("? Python environment is successfully set up!")
+print(f"NumPy Version: {np.__version__}")
+print(f"PyTorch Version: {torch.__version__}")
+print(f"Nibabel Version: {nib.__version__}")
+print(f"DOSMA Version: {dosma.__version__}")
+
